e6c3fb0edda4f859fad6374483232430e1f78f79
Pedro Hazki <pedrohazki.emp@gmail.com>
2026-01-15 23:15:50 -0300

    feat: setup express server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,12 @@
-console.log("Server starting...");
+const express = require("express")
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+    res.json({ message: "API is running" });
+});
+
+app.listen(PORT, () => {
+    console.log([`Server is running on port ${PORT}`]);
+});
